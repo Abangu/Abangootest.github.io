73238ddec5e20b34f20576a1cf8ae55feae7af41
Abangu <152433670+Abangu@users.noreply.github.com>
2026-03-23 21:52:34 +0200

    Add files via upload

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -2,8 +2,6 @@
 const input = document.getElementById("myInput");
 const myInput = parseInt(document.getElementById("myInput").value) - 1;
 const button = document.getElementById("playBtn");
-
-// const display = document.getElementById("output");
 const display1 = document.getElementById("myHeader");
 
 function updateWidth() {
@@ -81,11 +79,13 @@ input.addEventListener("input", async () => {
       display1.textContent = lines[maxValue - 1];
       document.getElementById("myInput").value = lines.length;
     }
-    document.getElementById("output1").textContent =
-      "video/" + input.value + ".webm";
+    // document.getElementById("output1").textContent =
+    //   "video/" + input.value + ".webm";
   } catch (error) {
     display1.textContent = "Ошибка: " + error.message;
+    // display1.textContent = lines[maxValue - 1];
   }
+  plcHld();
 });
 
 // ПЕРВАЯ КНОПКА
@@ -97,15 +97,12 @@ function readLineFirstButton() {
       // Разбиваем текст по переносам строк и считаем элементы
       const Flines = content.split(/\r\n|\r|\n/);
       let FmaxVal = Flines.length;
-
       document.getElementById("myInput").value = 1;
       document.getElementById("lineCount").textContent = " " + FmaxVal;
       // display.textContent = 1;
       display1.textContent = Flines[0];
-      // document.getElementById("outtest").innerText = FfilePath;
-      // document.getElementById("output1").textContent =
-      //   "video/" + input.value + ".webm";
     });
+  plcHld();
 }
 
 // НАЗАД КНОПКА
@@ -123,10 +120,8 @@ function readLinePrevButton() {
       document.getElementById("lineCount").textContent = " " + FmaxVal; // Количество строк:
       // display.textContent = myInput;
       display1.textContent = Flines[myInput - 1];
-      // document.getElementById("outtest").innerText = FfilePath;
-      // document.getElementById("output1").textContent =
-      //   "video/" + input.value + ".webm";
     });
+  plcHld();
 }
 
 // ПРОИЗВОЛЬНО КНОПКА
@@ -145,10 +140,8 @@ function readLineRandButton() {
       document.getElementById("lineCount").textContent = " " + FmaxVal;
       // display.textContent = randomNumber;
       display1.textContent = Flines[randomNumber - 1];
-      // document.getElementById("outtest").innerText = FfilePath;
-      // document.getElementById("output1").textContent =
-      //   "video/" + input.value + ".webm";
     });
+  plcHld();
 }
 
 // ВПЕРЁД КНОПКА
@@ -166,10 +159,8 @@ function readLineButton() {
       document.getElementById("lineCount").textContent = " " + FmaxVal; // Количество строк:
       // display.textContent = myInput;
       display1.textContent = Flines[myInput - 1];
-      // document.getElementById("outtest").innerText = FfilePath;
-      // document.getElementById("output1").textContent =
-      //   "video/" + input.value + ".webm";
     });
+  plcHld();
 }
 
 // ПОСЛЕДНЯЯ КНОПКА
@@ -186,10 +177,14 @@ function readLineLastButton() {
       document.getElementById("lineCount").textContent = " " + FmaxVal;
       // display.textContent = FmaxVal;
       display1.textContent = Flines[FmaxVal - 1];
-      // document.getElementById("outtest").innerText = FfilePath;
-      // document.getElementById("output1").textContent =
-      //   "video/" + input.value + ".webm";
     });
+  plcHld();
+}
+function plcHld() {
+  var video = document.getElementById("myVideo");
+  video.pause(); // 1. Остановить видео
+  video.setAttribute("poster", "images/placeholder.jpeg");
+  video.load(); // 4. Перезагрузить элемент, чтобы показать постер
 }
 
 button.addEventListener("click", function () {
